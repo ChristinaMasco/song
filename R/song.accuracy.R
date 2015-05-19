a974babdc33e730fcd60736ea675331335ac97f9
Christina Masco <cmasco@uchicago.edu>
2015-05-19 16:40:49 -0500

    Replaced for-loops with lapply where possible and streamlined

--- R/song.accuracy.R
+++ R/song.accuracy.R
@@ -1,70 +1,60 @@
-song.CountTotals <- function(birdlist){
-  total.songs <- numeric()
-  total.time <- numeric()
-  for (i in 1:length(birdlist)){
-    total.songs[i] <- birdlist[[i]][[1]]$songs.num + birdlist[[i]][[2]]$songs.num
-    total.time[i] <- birdlist[[i]][[1]]$end.record.time - birdlist[[i]][[1]]$start.record.time 
-  }
-  total <- data.frame(time=total.time, number=total.songs)
-  return(total)
-}
 
-
-song.TestAccuracy <- function(birdlist, model, overlap.function){
-  dx <- numeric()
-  pct <- numeric()
-  p <-numeric()
-  
+song.TestAccuracy <- function(indivlist, num.rand, overlap.function, 
+                              randomize.function){
+  ## calculate the observed and expected amounts of overlap
+  rndm <- song.BatchSimulate(indivlist, num.rand, overlap.function,
+                             randomize.function)
+  ## extract values from the results list
+  ## setting the first individual to sing as the reference individual
+  observed <- unlist(lapply(rndm, function(x) x$observed[2,1]))
+  expected <- unlist(lapply(rndm, function(x) x$expected[2,1]))
+  p.values <- unlist(lapply(rndm, function(x) x$p.values[2,1]))
+  ## count totals for % error calculation  
   if(identical(overlap.function,song.TimeOverlap)){
-    total <- song.CountTotals(birdlist)[,1]
+    ## if TimeOverlap, total = total duration of interaction
+    total <- unlist(lapply(indivlist, function(x) x[[1]]$end.record.time -
+                             x[[1]]$start.record.time))
   } else{
-    total <- song.CountTotals(birdlist)[,2]
+    ## if NumOverlap, total = total number of songs 
+    total <- unlist(lapply(indivlist, function(x) x[[1]]$songs.num + 
+                             x[[2]]$songs.num))
   }
-  
-  for (i in 1:length(model)){
-    mean <- model[[i]]$expected[2,1]
-    observed <- model[[i]]$observed[2,1]
-    
-    num.birds = length(dimnames(model[[i]]$observed)[[1]])
-    num.rand = length(model[[i]]$randomized)/num.birds^2
-    
-    dx[i] <- abs(observed - mean)
-    p[i] <- model[[i]]$p.values[2,1]
-  }
-  
+  ## error calculations
+  dx <- abs(observed - expected)
   avg.dx <- mean(dx)
   avg.pct <- mean(dx/total)
-  avg.p <- mean(p)
-  num.overlap <- sum(p < 0.05)
-  num.avoid <- sum(p > 0.95)
-  
-  output <- c(avg.dx,avg.pct,avg.p,num.overlap,num.avoid)
-  names(output) <- c("avg dev", "avg dev (%)", "avg P-value", "P < 0.05", "P > 0.95")
-  
+  var.p <- var(p.values)
+  num.overlap <- sum(p.values < 0.05)
+  num.avoid <- sum(p.values > 0.95)
+  ## build output
+  output <- c(avg.dx, avg.pct, var.p, num.overlap, num.avoid)
+  names(output) <- c("Average Error", "Average Error (%)", "Variance of P",
+                     "P < 0.05", "P > 0.95")
   return(output)
-  
 }
 
 ##Evaluate accuracy as a function of the number of randomizations
 ## n is a numeric vector specifying the number of randomizations 
 
-song.HowManyRandomizations <- function (birdlist, n, overlap.function, randomize.function){
-  tests <- data.frame(avg.dx=numeric(), avg.pct=numeric(), avg.p=numeric(), 
-                      num.overlap=numeric(), num.avoid=numeric())
-  runtime = numeric()
-    
+song.HowManyRndms <- function(indivlist, n, overlap.function, 
+                              randomize.function){
+  accuracy <- list()
+  runtime <- numeric()
   for (i in 1:length(n)){
-    # start the clock
+    ## start the clock
     ptm <- proc.time()
-    # run the simulation
-    model <- song.BatchSimulate(birdlist, n[i], overlap.function, randomize.function)
-    # stop the clock and store the elapsed time
+    ## run the simulation and test accuracy
+    accuracy[[i]] <- song.TestAccuracy(indivlist, n[i], overlap.function,
+                                       randomize.function)
+    ## stop the clock and store elapsed time
     run <- as.vector(proc.time() - ptm)
-    runtime[i] <- run[3]
-    # test the accuracy of the model
-    tests[i,] <- song.TestAccuracy(birdlist,model,overlap.function)
+    runtime[i] <- run[3]    
   }
-  # return a data frame containing the number of randomizations, the average % deviation, and the run time
-  output <- data.frame(n=n, dx=tests[,1], pct=tests[,2], p=tests[,3], runtime=runtime)
+  ## build output
+  results <- do.call(rbind, accuracy)
+  output <- data.frame(n=n, avg.error=results[,1], avg.pct=results[,2],
+                       var.p=results[,3], runtime=runtime)
+  names(output) <- c("N","Average Error", "Average Error (%)", 
+                     "Variance of P", "Run Time")
   return(output)
 }

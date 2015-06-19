302744c0cd62a89645947cf291da2dc99b3837c7
Christina Masco <cmasco@uchicago.edu>
2015-06-19 13:45:19 -0500

    Added documentation for sample data sets

---
+++ R/song.datasets.R
@@ -0,0 +1,58 @@
+#' A sample interaction from black-capped chickadees.
+#' 
+#' A data set containing the start and end times of each song produced during 
+#' an interaction between a black-capped chickadee and a fixed-interval 
+#' playback stimulus. These data are from a study by Baker, Wilson and Mennill
+#' (2013) designed to evaluate whether song overlap predicts the attack of a 
+#' rival in this species. 
+#' 
+#' @format A data frame with 50 rows and 3 variables
+#' 
+#' @source Baker TM, Wilson DR, Mennill DJ. 2013. Vocal signals predict attack
+#' during aggressive interactions in black-capped chickadees. Anim Behav. 84:
+#' 965-974.
+#' 
+#' @name chickadees
+#' 
+NULL
+
+#' A sample interaction from rufous-and-white wrens.
+#' 
+#' A data set containing the start and end times of each song produced during
+#' an interaction between a duetting pair of rufous-and-white wrens. These data
+#' were collected using an 8-channel microphone array designed to passively 
+#' sample the natural duetting behavior of breeding partners as they move 
+#' around their territories (Mennill et al. 2006; Mennill & Vehrencamp 2008).
+#' 
+#' @format A data frame with 50 rows and 3 variables
+#' 
+#' @source 
+#' Mennill D, Burt J, Fristrup K, Vehrencamp S. 2006. Accuracy of an acoustic 
+#' location system for monitoring the position of duetting songbirds in 
+#' tropical forest. J Acoust Soc Am. 119:2832-2839.
+#' 
+#' Mennill DJ, Vehrencamp SL. 2008. Context-dependent functions of avian duets 
+#' revealed by microphone-array recordings and multispeaker playback. Curr 
+#' Biol. 18:1314-1319.
+#' 
+#' @name wrens
+NULL
+
+#' A sample interaction from long-tailed manakins.
+#' 
+#' A data set containing the start and end times of each song produced during
+#' an interaction between neighboring male-male pairs of long-tailed manakins.
+#' These data are from a study by Maynard, Ward, Doucet, and Mennill (2012) 
+#' designed to evaluate whether male-male pairs vary the timing of their duets
+#' with respect to the duets of males in adjacent leks. 
+#' 
+#' @format A data frame with 50 rows and 3 variables
+#' 
+#' @source
+#' Maynard DF, Ward KA, Doucet SM, Mennill DJ. 2012. Calling in an acoustically
+#' competitive environment: duetting male long-tailed manakins avoid 
+#' overlapping neighbours but not playback-simulated rivals. Anim Behav. 
+#' 84:563-573.
+#' 
+#' @name manakins
+NULL

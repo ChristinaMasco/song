a3e5c319d622e4a81d0cf8ae93140501b3f9553e
Christina Masco <cmasco@uchicago.edu>
2015-10-07 09:19:26 +1030

    Minor edits to data set documentation.

--- R/song.R
+++ R/song.R
@@ -3,24 +3,24 @@
 #' A data set containing the start and end times of each song produced during
 #' an interaction between a black-capped chickadee and a fixed-interval
 #' playback stimulus. These data are from a study by Baker, Wilson and Mennill
-#' (2013) designed to evaluate whether song overlap predicts the attack of a
+#' (2012) designed to evaluate whether song overlap predicts the attack of a
 #' rival in this species.
 #'
 #' @format A data frame with 50 rows and 3 variables
 #'
-#' @source Baker TM, Wilson DR, Mennill DJ. 2013. Vocal signals predict attack
+#' @source Baker TM, Wilson DR, Mennill DJ. 2012. Vocal signals predict attack
 #' during aggressive interactions in black-capped chickadees. Anim Behav. 84:
 #' 965-974.
 #'
 "chickadees"
 
-#' An interaction between a female rufous-and-white wren and her mate.
+#' A duet interaction between a female rufous-and-white wren and her mate.
 #'
-#' A data set containing the start and end times of each song produced during
-#' an interaction between a duetting pair of rufous-and-white wrens. These data
-#' were collected using an 8-channel microphone array designed to passively
-#' sample the natural duetting behavior of breeding partners as they move
-#' around their territories (Mennill et al. 2006; Mennill & Vehrencamp 2008).
+#' A data set containing the start and end times of each song produced by a
+#' duetting pair of rufous-and-white wrens. These data were collected using an
+#' 8-channel microphone array designed to passively sample the natural
+#' duetting behavior of breeding partners as they move around their territories
+#' (Mennill et al. 2006; Mennill & Vehrencamp 2008).
 #'
 #' @format A data frame with 50 rows and 3 variables
 #'
